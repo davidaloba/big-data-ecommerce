fdee593a1c54f69abebbe9634bf7962089c4141c
mistertaavetti <webmaster@taavetti.digital>
2023-02-21 18:27:47 +0100

    [ts-migrate][.] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

---
+++ packages/app/global/components/layouts/layout.tsx
@@ -0,0 +1,46 @@
+import Footer from '@components/partials/Footer'
+import Navbar from '@components/partials/Navbar'
+import PreviewBanner from '@components/partials/PreviewBanner'
+import Seo from '@components/partials/seo'
+import BlockManager from '@components/blocks/BlockManager'
+import ModuleManager from '@modules/ModuleManager'
+
+const Layout = ({ children, global, page, pageDataIsSuccess, type, preview }) => {
+  const pageData = page.attributes
+  const { navigation, footer } = global.attributes
+
+  const blocks = pageData.blocks || []
+  const modules = pageData.main
+
+  return (
+    <div>
+      <Seo seo={pageData.seo} />
+      {preview && <PreviewBanner />}
+      {pageDataIsSuccess && (
+        <Navbar
+          navigation={navigation}
+          pageData={pageData}
+          type={type}
+          locale={pageData.locale}
+        />
+      )}
+      {children}
+      {modules && (
+        <ModuleManager
+          modules={modules}
+          locale={pageData.locale}
+          perPage={pageData.perPage || 12}
+        />
+      )}
+      {blocks && <BlockManager blocks={blocks} />}
+      {pageDataIsSuccess && (
+        <Footer
+          footer={footer}
+          locale={pageData.locale}
+        />
+      )}
+    </div>
+  )
+}
+
+export default Layout
