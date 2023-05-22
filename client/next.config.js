fdc330d843be9332cd25b7ce75d07e24f6287456
lampstead <vm-dev@taavetti.digital>
2023-05-22 23:29:29 +0100

    no-client-apk

--- client/next.config.js
+++ client/next.config.js
@@ -9,5 +9,14 @@ module.exports = {
       }
     ]
   },
-  experimental: {}
+  experimental: {},
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true
+  },
+  // output: 'standalone',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if  your project has ESLint errors.
+    ignoreDuringBuilds: false
+  }
 }
