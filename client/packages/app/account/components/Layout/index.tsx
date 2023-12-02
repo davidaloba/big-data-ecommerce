53402369020bc7015f506a28d2741b79fc91f27b
lampstead <vm-dev@taavetti.digital>
2023-12-02 09:51:30 +0100

    docker-compose-update

--- client/packages/app/account/components/Layout/index.tsx
+++ client/packages/app/account/components/Layout/index.tsx
@@ -11,13 +11,13 @@ export default function AccountLayout({ children }: { children: React.ReactNode 
 
   return (
     <section className="pb-2 md:pb-5 pt-10 px-5 md:px-20 lg:px-36">
-      <div className="flex flex-row items-stretch justify-between gap-12">
-        <div className=" w-1/5 ">
-          <div className="md:sticky md:top-20 w-full ">
+      <div className="flex flex-col md:flex-row items-stretch justify-between gap-12">
+        <div className=" w-full md:w-1/5 ">
+          <div className="sticky top-20">
             <AccountMenu />
           </div>
         </div>
-        <div className=" w-4/5 ">{children}</div>
+        <div className=" w-full md:w-4/5 ">{children}</div>
       </div>
     </section>
   )
